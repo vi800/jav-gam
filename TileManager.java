293bb1939ae657e8ae3e6b3811cfb10a0efa1da9
vi800 <virajparmar250@gmail.com>
2025-05-18 12:08:07 +0530

    Update TileManager.java

--- TileManager.java
+++ TileManager.java
@@ -1,17 +1,24 @@
 import javax.imageio.ImageIO;
 import java.awt.Graphics2D;
 import java.io.IOException;
+import java.io.File;
+import java.util.Scanner;
+import java.awt.image.BufferedImage;
 
 public class TileManager
 {
 	GamePanel gp;
 	Tile[] tile;
+	BufferedImage tileMap;
 
 	TileManager(GamePanel gp)
 	{
 		this.gp = gp;
-		tile = new Tile[2];
+		tile = new Tile[3];
 		getTileImage();
+		tileMap = new BufferedImage(gp.width, gp.height, BufferedImage.TYPE_3BYTE_BGR);
+
+		getTileMap(0);
 	}
 
 	void getTileImage()
@@ -19,20 +26,42 @@ public class TileManager
 		try {
 			tile[0] = new Tile();
 			tile[1] = new Tile();
+			tile[2] = new Tile();
 
 			tile[0].img = ImageIO.read(getClass().getResourceAsStream("/res/tile/grass.png"));
 
 			tile[1].img = ImageIO.read(getClass().getResourceAsStream("/res/tile/water.png"));
 			tile[1].layer = 2;
 
+			tile[2].img = ImageIO.read(getClass().getResourceAsStream("/res/tile/tree.png"));
+			tile[2].layer = 1;
+
 		} catch(IOException e) {
 			e.printStackTrace();
 		}
 	}
 
+	void getTileMap(int n)
+	{
+		Scanner s;
+		try {
+			s = new Scanner(new File("map/"+n));
+			for(int j=0; j<gp.col; j++) {
+				for(int i=0; i<gp.row; i++) {
+					int t = s.nextInt();
+					Graphics2D g = (Graphics2D)tileMap.createGraphics();
+					g.drawImage(tile[t].img, i*gp.tileSize, j*gp.tileSize, gp.tileSize, gp.tileSize, null);
+					g.dispose();
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
 	void draw(Graphics2D g)
 	{
-		g.drawImage(tile[0].img, 0, 0, gp.tileSize, gp.tileSize, null);
-		g.drawImage(tile[1].img, 200, 100, gp.tileSize, gp.tileSize, null);
+		g.drawImage(tileMap, 0, 0, gp.width, gp.height, null);
 	}
 }
